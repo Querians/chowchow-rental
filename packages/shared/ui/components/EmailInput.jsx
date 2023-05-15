0bdfc9f4ab49388d43f7bf88a8e61b4487996d8b
Nontawat <mix_sk139@hotmail.com>
2023-05-15 16:01:44 +0700

    chore(ui): format code

--- packages/shared/ui/components/EmailInput.jsx
+++ packages/shared/ui/components/EmailInput.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 export const EmailInput = () => {
   const [isValid, setIsValid] = useState(true);
   const reg =
-  /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+    /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
   const handleChange = (event) => {
     if (!(event.target instanceof HTMLInputElement)) return;
     const email = event.target.value;
@@ -23,7 +23,7 @@ export const EmailInput = () => {
           name="email"
           type="email"
           onInput={handleChange}
-          id='email'
+          id="email"
           className="mb-2.5 block w-full rounded-lg border-2 border-black bg-white p-2.5 text-sm"
           required
         />
@@ -36,6 +36,5 @@ export const EmailInput = () => {
         )}
       </div>
     </>
-  )
-  ;
+  );
 };
