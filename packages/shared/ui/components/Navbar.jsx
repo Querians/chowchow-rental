000f7b220b917b06d90e587fc962250f0b60276a
Parefair <kanyaluck.cc@gmail.com>
2023-05-24 01:32:22 +0700

    fix[ui]: add hamberger

--- packages/shared/ui/components/Navbar.jsx
+++ packages/shared/ui/components/Navbar.jsx
@@ -1,142 +1,72 @@
-import * as React from 'react';
+import { useState } from 'react';
 import Image from 'next/image';
 import SearchBar from './SearchBar';
 
 export const Navbar = () => {
+  
+  const [isShow, setShow] = useState(false)
+  const onClick = () => {
+    setShow(!isShow)
+  }
+
   return (
     <>
-      <nav className="w-full border-[#000000]  border-b-2 bg-[#C3A982] dark:bg-gray-900">
-        <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between p-4">
-          <a href="https://flowbite.com/" className="flex items-center">
-            <Image
-              src="/dog.svg"
-              className="mr-3 h-8"
-              alt="Flowbite Logo"
-              width="30"
-              height="30"
-            />
-            <span className="self-center whitespace-nowrap text-2xl font-bold pr-12 dark:text-white">
-              CHOWCHOW
-            </span>
-          </a>
-          <div className="flex items-center md:order-2">
-            <button
-              type="button"
-              className="mr-3 flex rounded-full bg-gray-800 text-sm focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600 md:mr-0"
-              id="user-menu-button"
-              aria-expanded="false"
-              datadropdowntoggle="user-dropdown"
-              datadropdownplacement="bottom"
-            >
-              <span className="sr-only">Open user menu</span>
-            </button>
-            <div
-              className="z-50 my-4 hidden list-none divide-y divide-gray-100 rounded-lg bg-white text-base shadow dark:divide-gray-600 dark:bg-gray-700"
-              id="user-dropdown"
-            >
-              <div className="px-4 py-3">
-                <span className="block text-sm text-gray-900 dark:text-white">
-                  Bonnie Green
+      <nav className="w-full bg-[#C3A982] dark:bg-gray-900">
+        <div className="w-full flex flex-wrap items-center justify-around p-4 m-0 border-[#000000]  border-b-2">
+          <ul className="flex flex-col p-4 align-baseline font-medium md:mt-0 md:flex-row md:space-x-8 md:border-0 md:p-0 ">
+            <li>
+              <a href="https://flowbite.com/" className="flex items-center">
+                <Image
+                  src="/dog.svg"
+                  className="mr-3 h-8"
+                  alt="Flowbite Logo"
+                  width="35"
+                  height="30"
+                />
+                <span className="self-center whitespace-nowrap text-2xl font-bold pr-12 dark:text-white">
+                  CHOWCHOW
                 </span>
-                <span className="block truncate  text-sm text-gray-500 dark:text-gray-400">
-                  name@flowbite.com
-                </span>
-              </div>
-              <ul className="py-2" aria-labelledby="user-menu-button">
-                <li>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white"
-                  >
-                    Dashboard
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white"
-                  >
-                    Settings
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white"
-                  >
-                    Earnings
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white"
-                  >
-                    Sign out
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <button
-              datacollapsetoggle="mobile-menu-2"
-              type="button"
-              className="ml-1 inline-flex items-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 md:hidden"
-              aria-controls="mobile-menu-2"
-              aria-expanded="false"
-            >
-              <span className="sr-only">Open main menu</span>
-              <svg
-                className="h-6 w-6"
-                aria-hidden="true"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-            </button>
-          </div>
-          <div
-            className="hidden w-full items-center justify-self-end md:order-1 md:flex md:w-auto"
-            id="mobile-menu-2"
+              </a>
+            </li>
+          </ul>
+
+          {/* hamberger */}
+          <button onClick={onClick} data-collapse-toggle="navbar-hamburger" type="button" class="md:hidden inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-hamburger" aria-expanded="false">
+            <span class="sr-only">Open main menu</span>
+            <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+          </button>
+          <div className="hidden w-full items-center justify-self-start md:order-1 md:flex md:w-auto"
+            id="mobile-menu-1"
           >
-            <ul className="mt-4 flex flex-col border border-gray-100 p-4 align-between font-medium dark:border-gray-700 dark:bg-gray-800 md:mt-0 md:flex-row md:space-x-8 md:border-0 md:p-0 md:dark:bg-gray-900">
-              <li>
-                <a
-                  href="#"
-                  className="block rounded bg-black-700 py-2 pl-3 pr-4 text-white md:bg-transparent md:p-0 md:text-[#000000] md:dark:text-blue-500"
-                  aria-current="page"
-                >
-                  PRODUCTS
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="block rounded py-2 pl-3 pr-4 text-gray-900 hover:bg-gray-100 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
-                >
-                  ABOUT US
-                </a>
-              </li>
-            </ul>
+            <div className="mt-4 flex flex-col border border-gray-100 p-4 align-between font-medium dark:border-gray-700 dark:bg-gray-800 md:mt-0 md:flex-row md:space-x-8 md:border-0 md:p-0 md:dark:bg-gray-900">
+              <a
+                href="/products"
+                className="block rounded py-2 pl-3 pr-4 text-gray-900 hover:text-slate-50"
+                aria-current="page"
+              >
+                PRODUCTS
+              </a>
+              <a
+                href=""
+                className="block rounded py-2 pl-3 pr-4 text-gray-900 hover:text-slate-50"
+              >
+                ABOUT US
+              </a>
+            </div>
           </div>
           <div
             className="hidden w-full items-end justify-between md:order-1 md:flex md:w-auto"
-            id="mobile-menu-3">
+            id="mobile-menu-2">
             <ul className="mt-4 flex flex-col border border-gray-100 p-4 align-baseline font-medium dark:border-gray-700 dark:bg-gray-800 md:mt-0 md:flex-row md:space-x-8 md:border-0 md:p-0 md:dark:bg-gray-900">
-            <li>
+              {/* <li className='w-[80%]'>
                 <SearchBar />
-              </li>
+              </li> */}
               <li>
                 <a href="/profile" className="flex items-center">
                   <Image
                     src="/profile.svg"
                     className="mr-3 h-8"
-                    alt="Flowbite Logo"
+                    alt="Profile Logo"
                     width="25"
                     height="25"
                   />
@@ -147,7 +77,7 @@ export const Navbar = () => {
                   <Image
                     src="/cart.svg"
                     className="mr-3 h-8"
-                    alt="Flowbite Logo"
+                    alt="Cart Logo"
                     width="25"
                     height="25"
                   />
@@ -155,7 +85,24 @@ export const Navbar = () => {
               </li>
             </ul>
           </div>
-       </ div>
+        </ div>
+        {isShow &&
+            <div class="w-full md:hidden" id="navbar-hamburger">
+            <ul class="flex flex-col font-medium bg-[#8E7F68]">
+              <li>
+                <a href="/products" class="block py-2 text-center  text-gray-900 hover:bg-gray-100">PRODUCT</a>
+              </li>
+              <li>
+                <a href="/profile" class="block bg-[#C3A982] py-2 text-center text-gray-900 hover:bg-gray-100">PROFILE</a>
+              </li>
+              <li>
+                <a href="/cart" class="block py-2 text-center text-gray-900 hover:bg-gray-100">CART</a>
+              </li>
+              <li>
+                <a href="" class="block bg-[#C3A982]  py-2 text-center text-gray-950 hover:bg-gray-100">ABOUT US</a>
+              </li>
+            </ul>
+          </div>}
       </nav>
     </>
   );
