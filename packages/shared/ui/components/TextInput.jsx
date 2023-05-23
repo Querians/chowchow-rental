65b4d3b904ad071aef73b582d176b88e5b115723
Nontawat <mix_sk139@hotmail.com>
2023-05-23 22:27:59 +0700

    feat(ui): add TextInput

--- packages/shared/ui/components/TextInput.jsx
+++ packages/shared/ui/components/TextInput.jsx
@@ -1,5 +1,29 @@
-import * as React from 'react';
-
-export const TextInput = ({ title }) => {
-  return <></>;
+export const TextInput = ({
+  onChange,
+  value,
+  placeholder,
+  label,
+  isRequire = true,
+}) => {
+  return (
+    <>
+      <label
+        htmlFor={label}
+        className="mb-2.5 block text-sm font-medium text-black"
+      >
+        {label}
+      </label>
+      <input
+        name={label}
+        type="text"
+        value={value}
+        autoComplete="true"
+        onChange={onChange}
+        id={label}
+        className="mb-2.5 block w-full rounded-md border-2 border-black p-2.5 text-sm "
+        placeholder={placeholder}
+        required={isRequire}
+      />
+    </>
+  );
 };
