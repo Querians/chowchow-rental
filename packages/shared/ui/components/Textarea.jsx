37d750e099e59ba976ef1eb8d429582cd47dfb6c
Nontawat <mix_sk139@hotmail.com>
2023-05-24 14:48:21 +0700

    feat(ui): add Textarea

---
+++ packages/shared/ui/components/Textarea.jsx
@@ -0,0 +1,28 @@
+export const Textarea = ({
+  onChange,
+  value,
+  placeholder,
+  label,
+  isRequire = true,
+}) => {
+  return (
+    <>
+      <label
+        htmlFor={label}
+        className="mb-2.5 block text-sm font-medium text-black dark:text-white"
+      >
+        {label}
+      </label>
+      <textarea
+        name={label}
+        id={label}
+        rows="4"
+        onChange={onChange}
+        value={value}
+        placeholder={placeholder}
+        className="block w-full rounded-md border-2 border-black p-2.5 text-sm focus:border-blue-500 focus:ring-blue-500"
+        required={isRequire}
+      />
+    </>
+  );
+};
