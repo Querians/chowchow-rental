4d245a9f303218877558da3289c2e3a468468022
Nontawat <mix_sk139@hotmail.com>
2023-05-25 02:43:56 +0700

    chore(phone input): format code

--- packages/shared/ui/components/PhoneInput.jsx
+++ packages/shared/ui/components/PhoneInput.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-export const PhoneInput = ({ onChange, value, isRequire=true }) => {
+export const PhoneInput = ({ onChange, value, isRequire = true }) => {
   const [isValid, setIsValid] = useState(true);
 
   const handleChange = (e) => {
@@ -11,30 +11,30 @@ export const PhoneInput = ({ onChange, value, isRequire=true }) => {
 
   return (
     <>
-       <label
-          htmlFor='Phone Number'
-          className="mb-2.5 block text-sm font-medium text-black"
-        >
-          Phone Number
-        </label>
-        <input
-        name='Phone Number'
-        id='Phone Number'
+      <label
+        htmlFor="Phone Number"
+        className="mb-2.5 block text-sm font-medium text-black"
+      >
+        Phone Number
+      </label>
+      <input
+        name="Phone Number"
+        id="Phone Number"
         type="tel"
         onInput={handleChange}
         onChange={onChange}
         value={value}
-        className='mb-2.5 block w-full rounded-md border-2 border-black p-2.5 text-sm '
-        placeholder='0123456789'
+        className="mb-2.5 block w-full rounded-md border-2 border-black p-2.5 text-sm "
+        placeholder="0123456789"
         required={isRequire}
-        />
-        <span
-          className={
-            'text-sm text-[#C26666] ' + (!isValid ? 'visible' : 'invisible')
-          }
-        >
-          The Phone Number you entered is incorrect format.
-        </span>
+      />
+      <span
+        className={
+          'text-sm text-[#C26666] ' + (!isValid ? 'visible' : 'invisible')
+        }
+      >
+        The Phone Number you entered is incorrect format.
+      </span>
     </>
   );
 };
