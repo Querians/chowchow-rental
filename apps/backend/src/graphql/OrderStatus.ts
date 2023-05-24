1d3790f378b0f1ee58dc0817d5530bdd7f9d142f
Qandle <unmistakework@hotmail.com>
2023-05-25 03:04:32 +0700

    feat(backend): define orderStatus and query all
    Co-authored-by: QuartzQw <QuartzQw@users.noreply.github.com>

--- apps/backend/src/graphql/OrderStatus.ts
+++ apps/backend/src/graphql/OrderStatus.ts
@@ -6,39 +6,47 @@ export const OrderStatus = objectType({
   definition(t) {
     t.nonNull.int('statusCode');
     t.nonNull.string('statusDef');
+    t.list.field('orders', {
+      type: 'Order',
+      resolve(parent, args, context: Context){
+        return context.prisma.orderStatus.findUnique({
+          where: { statusCode: parent.statusCode }
+        }).orders();
+      }
+    })
   },
 });
 
 export const OrderStatusQuery = extendType({
   type: "Query",
   definition(t) {
-      t.nonNull.list.nonNull.field("allorder", {
+      t.list.field("allOrderStatus", {
           type: "OrderStatus",
           resolve(parent, args, context, info) {
               return context.prisma.orderStatus.findMany();
-          },
+          }
       });
   },
 });
 
-export const OrderStatusMutation = extendType({
-  type: 'Mutation',
-  definition(t) {
-    t.nonNull.field("addStage", {
-      type: 'OrderStatus',
-      args: {
-        statusCode: nonNull(intArg()),
-        statusDef: nonNull(stringArg())
-      },
-      resolve(parent, args: any, context: Context){
-        const newStage = context.prisma.orderStatus.create({
-          data: {
-            statusCode: args.statusCode,
-            statusDef: args.statusDef
-          }
-        })
-        return newStage;
-      }
-    })
-  }
-});
+// export const OrderStatusMutation = extendType({
+//   type: 'Mutation',
+//   definition(t) {
+//     t.nonNull.field("addStage", {
+//       type: 'OrderStatus',
+//       args: {
+//         statusCode: nonNull(intArg()),
+//         statusDef: nonNull(stringArg())
+//       },
+//       resolve(parent, args: any, context: Context){
+//         const newStage = context.prisma.orderStatus.create({
+//           data: {
+//             statusCode: args.statusCode,
+//             statusDef: args.statusDef
+//           }
+//         })
+//         return newStage;
+//       }
+//     })
+//   }
+// });
