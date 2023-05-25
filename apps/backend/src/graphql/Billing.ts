7ca20c28d3745ad9bfa7829cc31be66b19ac5645
Qandle <unmistakework@hotmail.com>
2023-05-25 20:16:08 +0700

    fix(backend): define Billing composite pk
    Co-authored-by: QuartzQw <QuartzQw@users.noreply.github.com>

--- apps/backend/src/graphql/Billing.ts
+++ apps/backend/src/graphql/Billing.ts
@@ -7,27 +7,35 @@ export const Billing = objectType({
     t.nonNull.string('billingId');          // PK1
     t.nonNull.dateTime('billTimestamp');    // PK2 (timestamp)
     // t.nonNull.string('invoiceId');          // FK1 from Invoice
-    t.nonNull.field('invoice', {
+    t.field('invoice', {
       type: 'Invoice',
       resolve(parent, args, context: Context){
-        return context.prisma.billing.findUnique({
-          where: { billpk: {
-            billingId: parent.billingId,
-            billTimestamp: parent.billTimestamp
-          } }
-        }).invoice();
+        return context.prisma.billing
+        .findUnique({
+          where: {
+            billingId_billTimestamp: {
+              billingId: parent.billingId,
+              billTimestamp: parent.billTimestamp
+            }
+           }
+        })
+        .invoice();
       }
     })
     // t.nonNull.string('paymentMethodId');    // FK2 from PaymentMethod
     t.nonNull.field('paymentMethod', {
       type: 'PaymentMethod',
       resolve(parent, args, context: Context){
-        return context.prisma.billing.findUnique({
-          where: { billpk: {
-            billingId: parent.billingId,
-            billTimestamp: parent.billTimestamp
-          } }
-        }).paymentMethod();
+        return context.prisma.billing
+        .findUnique({
+          where: {
+            billingId_billTimestamp: {
+              billingId: parent.billingId,
+              billTimestamp: parent.billTimestamp
+            }
+           }
+        })
+        .paymentMethod();
       }
     })
     t.nonNull.float('paidAmount');          // (double)
