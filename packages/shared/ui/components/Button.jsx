a0009df29a1bf5c2b9ab5e78edf9dec0cafa5289
Parefair <kanyaluck.cc@gmail.com>
2023-05-26 00:40:46 +0700

    fix(ui): Responsive detail

--- packages/shared/ui/components/Button.jsx
+++ packages/shared/ui/components/Button.jsx
@@ -6,7 +6,7 @@ export const Button = ({ logo, onClick, text, type }) => {
     <>
       {type == 'normal' ? (
         <button
-          className="mb-2 mr-2 h-10 w-full rounded-md border border-2 border-neutral-900 bg-[#C3A982] px-5 py-2 text-sm font-medium text-gray-900 hover:bg-[#89724E] hover:text-white"
+          className="mb-2 mr-2 h-fit w-full rounded-md border border-2 border-neutral-900 bg-[#C3A982] px-5 py-2 text-sm font-medium text-gray-900 hover:bg-[#89724E] hover:text-white"
           onClick={onClick}
         >
           {logo ? <img src={logo} alt="" className="h-8" /> : <></>}
@@ -17,7 +17,7 @@ export const Button = ({ logo, onClick, text, type }) => {
       )}
       {type == 'submit' ? (
         <button
-          className="mb-2 mr-2 h-10 w-full rounded-md border border-2 border-neutral-900 bg-[#A4DAAC] px-5 py-2 text-sm font-medium text-gray-900 hover:bg-[#54825B] hover:text-white"
+          className="mb-2 mr-2 h-fit w-full rounded-md border border-2 border-neutral-900 bg-[#A4DAAC] px-5 py-2 text-sm font-medium text-gray-900 hover:bg-[#54825B] hover:text-white"
           onClick={onClick}
         >
           {logo ? <img src={logo} alt="" className="h-8" /> : <></>}
@@ -28,7 +28,7 @@ export const Button = ({ logo, onClick, text, type }) => {
       )}
       {type == 'cancel' ? (
         <button
-          className="mb-2 mr-2 h-10 w-full rounded-md border border-2 border-neutral-900 bg-[#C26666] px-5 py-2 text-sm font-medium text-gray-900 hover:bg-[#8B3B3B] hover:text-white"
+          className="mb-2 mr-2 h-fit w-full rounded-md border border-2 border-neutral-900 bg-[#C26666] px-5 py-2 text-sm font-medium text-gray-900 hover:bg-[#8B3B3B] hover:text-white"
           onClick={onClick}
         >
           {logo ? <img src={logo} alt="" className="h-8" /> : <></>}
