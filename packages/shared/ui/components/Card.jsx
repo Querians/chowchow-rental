49fb1fccf65cf5bbc1b022682c3f9ecb53b3c2e7
Parefair <92637938+Parefair@users.noreply.github.com>
2023-05-25 20:45:31 +0700

    feat(UI):button (#11)
    * feat(ui): create button component
    
    * fix(ui): add onClick and logo
    
    * feat(ui): create navbar
    
    * feat(ui) : create search bar file
    
    * fix(ui): add element searchbar in navbar
    
    * fix: add search bar box
    
    * fix[ui]: add hamberger
    
    * fix[ui]: edit export
    
    * feat[ui]: Pagination
    
    * feat[ui]: Breadcrumb
    
    * feat[ui]: create card
    
    * fix[ui]: add link
    
    * feat[ui]: sidebar complete & format
    
    * fix[ui]: Navbar edit logo size and format
    
    * Fix[ui]: Edit parameter & format
    
    * Fix[ui]: format searchbar
    
    * Fix[ui]: card edit layout & format
    
    * Fix[ui]: Pagination detail & format but not prop

--- packages/shared/ui/components/Card.jsx
+++ packages/shared/ui/components/Card.jsx
@@ -1,5 +1,41 @@
 import * as React from 'react';
+import Image from 'next/image';
+import { Button } from './Button';
+import Link from 'next/link';
 
-export const Card = ({ type }) => {
-  return <></>;
+export const Card = ({ productName, detail, price, pic, link }) => {
+  return (
+    <>
+      <div class="max-w-xs rounded-lg border border-gray-200 bg-white shadow ring-[#C3A982] hover:ring-2">
+        <div class="p-5">
+          <div class="flex justify-between">
+            <div class="">
+              <h5 class="mb-2 text-2xl font-bold text-gray-900">
+                {productName}
+              </h5>
+            </div>
+            <div class="">
+              <p class="mb-3 text-2xl font-semibold text-red-600">
+                {price} ฿/day
+              </p>
+            </div>
+          </div>
+          <p class="mb-3 text-lg font-normal text-gray-700">{detail}</p>
+          <Image
+            src={pic}
+            className="w-full rounded-t-lg pb-2"
+            alt={productName}
+            width={200}
+            height={200}
+            quality={100}
+          />
+          <Link href={link}>
+            <Button text={'More Detail'} />
+          </Link>
+
+          {/* <Button text="More Details"/> */}
+        </div>
+      </div>
+    </>
+  );
 };
