64431a2e45f2e077b98f55ca5d490460151e7d28
Parefair <kanyaluck.cc@gmail.com>
2023-05-25 17:22:58 +0700

    Fix[ui]: card edit layout & format

--- packages/shared/ui/components/Card.jsx
+++ packages/shared/ui/components/Card.jsx
@@ -1,26 +1,40 @@
 import * as React from 'react';
 import Image from 'next/image';
+import { Button } from './Button';
+import Link from 'next/link';
 
-export const Card = ({ productName, detail, price, link }) => {
+export const Card = ({ productName, detail, price, pic, link }) => {
   return (
     <>
       <div class="max-w-xs rounded-lg border border-gray-200 bg-white shadow ring-[#C3A982] hover:ring-2">
-        <a href={link}>
-          <img class="rounded-t-lg" src="/ikea_black_chair.png" alt="" />
-          <div class="p-5">
-            <a href="#">
-              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+        <div class="p-5">
+          <div class="flex justify-between">
+            <div class="">
+              <h5 class="mb-2 text-2xl font-bold text-gray-900">
                 {productName}
               </h5>
-            </a>
-            <p class="mb-3 font-normal text-gray-700">{detail}</p>
-            <p class="mb-3 text-lg font-semibold text-red-600">{price} ฿/day</p>
-            {/* <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-            Read more
-            <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-          </a> */}
+            </div>
+            <div class="">
+              <p class="mb-3 text-2xl font-semibold text-red-600">
+                {price} ฿/day
+              </p>
+            </div>
           </div>
-        </a>
+          <p class="mb-3 text-lg font-normal text-gray-700">{detail}</p>
+          <Image
+            src={pic}
+            className="w-full rounded-t-lg pb-2"
+            alt={productName}
+            width={200}
+            height={200}
+            quality={100}
+          />
+          <Link href={link}>
+            <Button text={'More Detail'} />
+          </Link>
+
+          {/* <Button text="More Details"/> */}
+        </div>
       </div>
     </>
   );
