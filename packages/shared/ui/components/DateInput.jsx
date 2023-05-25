cd95cca7ac8d5e058006e5bcc1785a831fe402f3
nontaxim <89908219+nontaxim@users.noreply.github.com>
2023-05-25 21:03:01 +0700

    feat(ui): input (#12)
    * feat(ui): add EmailInput
    
    * chore(ui): format code
    
    * style(email input): change text,bg color and rounded input
    
    * fix(email input): add onChange, value
    
    * chore(email input): format code
    
    * feat(ui): add PasswordInput
    
    * fix(email input): add placeholder
    
    * fix(password input): add placeholder
    
    * fix(password input): change regex
    
    * feat(ui): add TextInput
    
    * feat(ui): add Textarea
    
    * style(Textarea): add margin bottom
    
    * feat(ui): add Dropdown
    
    * feat(ui): add Map
    
    * fix(map): edit useEffect
    
    * feat(ui): add DateInput
    
    * feat(ui): add PhoneInput
    
    * chore(phone input): format code
    
    * fix(ui): add div
    
    * style(Map): change hight
    
    * feat(ui): add DropdownCard
    
    * feat(ui): add confirmInfo
    
    * feat(ui): add orderForm page

--- packages/shared/ui/components/DateInput.jsx
+++ packages/shared/ui/components/DateInput.jsx
@@ -1,5 +1,20 @@
-import * as React from 'react';
-
-export const DateInput = () => {
-  return <></>;
+export const DateInput = ({ onChange, label, isRequire = true }) => {
+  return (
+    <div>
+      <label
+        htmlFor={label}
+        className="mb-2.5 block text-sm font-medium text-black"
+      >
+        {label}
+      </label>
+      <input
+        name={label}
+        id={label}
+        type="date"
+        onChange={onChange}
+        className=" mb-2.5 inline-block w-full rounded-md border-2 border-black p-2.5 text-sm "
+        required={isRequire}
+      />
+    </div>
+  );
 };
--- packages/shared/ui/components/Dropdown.jsx
+++ packages/shared/ui/components/Dropdown.jsx
@@ -1,5 +1,32 @@
-import * as React from 'react';
-
-export const Dropdown = ({ type }) => {
-  return <></>;
+export const Dropdown = ({
+  label,
+  placeholder,
+  options,
+  onChange,
+  isRequire = true,
+}) => {
+  return (
+    <div>
+      <label
+        htmlFor={label}
+        className="mb-2.5 block text-sm font-medium text-black"
+      >
+        {label}
+      </label>
+      <select
+        name={label}
+        id={label}
+        onChange={onChange}
+        className="mb-2.5 block w-full rounded-md border-2 border-black p-2.5 text-sm text-black invalid:text-gray-500 focus:border-blue-500 focus:ring-blue-500"
+        required={isRequire}
+      >
+        <option hidden disabled selected value={''}>
+          {placeholder}
+        </option>
+        {Object.keys(options).map((key) => (
+          <option value={key}>{options[key]}</option>
+        ))}
+      </select>
+    </div>
+  );
 };
