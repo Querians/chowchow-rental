87f799f8e976e4c0442d5350caf7430c516082b5
Qandle <unmistakework@hotmail.com>
2023-05-27 01:54:16 +0700

    feat(api): setting apollo client

---
+++ apps/frontend/apollo-client.js
@@ -0,0 +1,17 @@
+import {
+  ApolloProvider,
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache
+} from '@apollo/client';
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000'
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
+
+export default client;
