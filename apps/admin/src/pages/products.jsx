107347c8169219861ca7319b93ecae691282f6b8
Parefair <kanyaluck.cc@gmail.com>
2023-05-27 13:55:10 +0700

    fix(ui): fix popup

--- apps/admin/src/pages/products.jsx
+++ apps/admin/src/pages/products.jsx
@@ -80,14 +80,14 @@ const Products = () => {
             description: "ใครมันคิดจะทำโปรเจคเรื่องนี้วะ"
         },
     }
-    const drop = () => {
-        popup();
-        // code for drop row
-    }
     const [isShow, setShow] = useState(false);
     const popup = () => {
         setShow(!isShow);
     };
+    const drop = () => {
+        popup();
+        // code for drop row
+    }
 
     return (
         <>
@@ -161,6 +161,26 @@ const Products = () => {
                         </div>
                     </div>
                 </div> */}
+                {isShow && (
+                    <div id="alert-additional-content-2" class="p-4 mb-4 text-red-800 border border-red-300 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400 dark:border-red-800" role="alert">
+                        <div class="flex items-center">
+                            <svg aria-hidden="true" class="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
+                            <span class="sr-only">Info</span>
+                            <h3 class="text-lg font-medium">This is a danger alert</h3>
+                        </div>
+                        <div class="mt-2 mb-4 text-sm">
+                            Are you sure to delete this row. If not please click "Exit" and if you want to delete please click "Delete".
+                        </div>
+                        <div class="flex">
+                            <button type="button" onClick={popup} class="text-white bg-red-800 hover:bg-red-900 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-xs px-3 py-1.5 mr-2 text-center inline-flex items-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
+                                Exit
+                            </button>
+                            <button type="button" onClick={drop} class="text-red-800 bg-transparent border border-red-800 hover:bg-red-900 hover:text-white focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-xs px-3 py-1.5 text-center dark:hover:bg-red-600 dark:border-red-600 dark:text-red-500 dark:hover:text-white dark:focus:ring-red-800" data-dismiss-target="#alert-additional-content-2" aria-label="Close">
+                                Delete
+                            </button>
+                        </div>
+                    </div>
+                )}
                 <div className="w-full rounded-lg border border-2 border-black p-4">
                     <h1 className="text-xl font-bold">Products</h1>
                     <div className="pt-2 px-4">
@@ -218,26 +238,6 @@ const Products = () => {
                                         )}
                                     </tr>
                                 </thead>
-                                {isShow && (
-                                    <div id="alert-additional-content-2" class="p-4 mb-4 text-red-800 border border-red-300 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400 dark:border-red-800" role="alert">
-                                        <div class="flex items-center">
-                                            <svg aria-hidden="true" class="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
-                                            <span class="sr-only">Info</span>
-                                            <h3 class="text-lg font-medium">This is a danger alert</h3>
-                                        </div>
-                                        <div class="mt-2 mb-4 text-sm">
-                                            Are you sure to delete this row. If not please click "Exit" and if you want to delete please click "Delete".
-                                        </div>
-                                        <div class="flex">
-                                            <button type="button" onClick={popup} class="text-white bg-red-800 hover:bg-red-900 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-xs px-3 py-1.5 mr-2 text-center inline-flex items-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
-                                                Exit
-                                            </button>
-                                            <button type="button" onClick={drop} class="text-red-800 bg-transparent border border-red-800 hover:bg-red-900 hover:text-white focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-xs px-3 py-1.5 text-center dark:hover:bg-red-600 dark:border-red-600 dark:text-red-500 dark:hover:text-white dark:focus:ring-red-800" data-dismiss-target="#alert-additional-content-2" aria-label="Close">
-                                                Delete
-                                            </button>
-                                        </div>
-                                    </div>
-                                )}
                                 <tbody>
                                     {Object.keys(data).map((key) => (
                                         <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
