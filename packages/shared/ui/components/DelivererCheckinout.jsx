8f9a88ab54a7f5ecbce8211524cd92c72ed27af9
Nontawat <mix_sk139@hotmail.com>
2023-05-27 23:57:23 +0700

    fix(Deliverer checkinout): edit object receiveData

--- packages/shared/ui/components/DelivererCheckinout.jsx
+++ packages/shared/ui/components/DelivererCheckinout.jsx
@@ -12,21 +12,21 @@ export const DelivererCheckinout = () => {
   const receiveData = {
     staff_id: 'sm6fk4f52ut',
     this_delivery_work_not_done: {
-      'order_id_1': 'order_id_1',
-      'order_id_2': 'order_id_2',
-      'order_id_3': 'order_id_3',
-      'order_id_4': 'order_id_4',
-      'order_id_5': 'order_id_5',
+      '1': 'order_id_1',
+      '2': 'order_id_2',
+      '3': 'order_id_3',
+      '4': 'order_id_4',
+      '5': 'order_id_5',
     }
   }
-
+console.log(returnData)
 
   return <>
       <h1 className="text-4xl font-bold py-6">Add New Category</h1>
       <form onSubmit={e => e.preventDefault()}>
         <div className="w-full rounded-lg border border-2 border-black p-4 mb-10">
             <div className="grid grid-cols-2 gap-x-12">
-                <Dropdown options={receiveData.this_delivery_work_not_done} label="Order ID" onChange={e => setReturnData({...returnData, ['order_id']:e.target.value})}/>
+                <Dropdown options={receiveData.this_delivery_work_not_done} label="Order ID" onChange={e => setReturnData({...returnData, ['order_id']:receiveData.this_delivery_work_not_done[e.target.value]})}/>
                 <TextInput type={'readOnly'} label={'Staff ID'} value={receiveData.staff_id}/>
                 <Button type="submit" text="Check In" onClick={e => setReturnData({...returnData, ['time_back']:new Date(), ['order_status']:232})}/>
                 <Button type="submit" text="Check Out" onClick={e => setReturnData({...returnData, ['time_go']:new Date(), ['order_status']:231})} />
