b5a1083bdf79510c31114b6d9854268f5087bb02
Parefair <kanyaluck.cc@gmail.com>
2023-05-28 17:51:42 +0700

    fix(ui): side bar

--- packages/shared/ui/components/Sidebar.jsx
+++ packages/shared/ui/components/Sidebar.jsx
@@ -470,7 +470,7 @@ export const Sidebar = ({ fullName, staffID, role, showStock, showOrder, showFin
                     </li>
                     <li>
                       <a
-                        href="/catagoryproblem"
+                        href="/categoryproblem"
                         class="group flex w-full items-center rounded-lg p-2 pl-11 text-gray-900 transition duration-75 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
                       >
                         Category Problems
